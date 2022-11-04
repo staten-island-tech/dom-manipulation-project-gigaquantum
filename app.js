8b8cfc082ddf94be93e826353a41f0a528ddb664
AntiProSkills <113395660+gigaquantum@users.noreply.github.com>
2022-11-04 13:15:51 -0400

    update

--- app.js
+++ app.js
@@ -1,25 +1,51 @@
 const DOMSelectors = {
-  brand: document.getElementById("brand"),
-  model: document.getElementById("model"),
-  year: document.getElementById("year"),
-  owners: document.getElementById("owners"),
-  milesDriven: document.getElementById("miles-driven"),
+  info: {
+    brand: document.getElementById("brand"),
+    model: document.getElementById("model"),
+    year: document.getElementById("year"),
+    owners: document.getElementById("owners"),
+    milesDriven: document.getElementById("miles-driven"),
+  },
   reset: document.getElementById("reset"),
   submit: document.getElementById("submit"),
   inputFields: document.querySelectorAll(".input"),
 };
 
-console.log(DOMSelectors.inputFields)
+console.log(DOMSelectors.inputFields);
 
 function resetInputs(inputArray) {
-  inputArray.forEach(element => {
+  inputArray.forEach((element) => {
     element.value = "";
-    console.log(element)
   });
 }
 
-function 
+function resetResults() {
+  // Work in progress
+}
 
-DOMSelectors.reset.addEventListener("click", function() {
+/* function getInfo(fetchedInfo) {
+  const infoObj = fetchedInfo.value;
+  console.log(infoObj);
+} */
+
+DOMSelectors.reset.addEventListener("click", function () {
   resetInputs(DOMSelectors.inputFields);
 });
+
+DOMSelectors.submit.addEventListener("click", function () {
+  getInfo(DOMSelectors.inputFields);
+});
+
+/* Trying to make a function to gather any inputs
+const obj = {
+  name: "Jean-Luc Picard",
+  rank: "Captain",
+};
+
+let objTwo = {};
+
+// Prints "name Jean-Luc Picard" followed by "rank Captain"
+Object.entries(obj).forEach((entry) => {
+  objTwo.(`${entry[0]}`) = (`${entry[1]}`);
+  console.log(entry[0]);
+}); */
